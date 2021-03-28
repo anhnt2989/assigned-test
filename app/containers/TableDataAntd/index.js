a1dca4e9fe880d8b25fbd8df53702d2f12a1ba0a
Chubby-Dev <anhnt2989@gmail.com>
2021-03-28 15:29:45 +0700

    [Ant Data Table] - temporary Completed

--- app/containers/TableDataAntd/index.js
+++ app/containers/TableDataAntd/index.js
@@ -15,14 +15,22 @@ import Input from 'antd/lib/input';
 import Row from 'antd/lib/row';
 import Col from 'antd/lib/col';
 import Slider from 'antd/lib/slider';
+import Button from 'antd/lib/button';
+import Modal from 'antd/lib/modal';
+import Form from 'antd/lib/form';
+import Select from 'antd/lib/select';
 import { SearchOutlined } from '@ant-design/icons';
 import ReactDragListView from "react-drag-listview";
 import NumberFormat from 'react-number-format';
-import { filter, includes, isEmpty, maxBy, minBy } from 'lodash';
+import { filter, includes, isEmpty, maxBy, minBy, isEqual } from 'lodash';
+import { ToastContainer, toast } from 'react-toastify';
+import uuid from 'react-uuid';
 
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
 import MockData from 'constants/mockup';
+
+import AntdFormInput from 'components/AntdFormInput';
 
 import makeSelectTableDataAntd from './selectors';
 import reducer from './reducer';
@@ -49,6 +57,7 @@ const columns = [
   {
     title: 'Origin',
     dataIndex: 'origin',
+    sorter: (a, b) => a.origin.localeCompare(b.origin),
   },
   {
     title: 'Actions',
@@ -81,7 +90,7 @@ function DraggableTable(props) {
         onDragEnd={onDragEnd}
         nodeSelector="th"
       >
-      <Table bordered columns={innerColumns} dataSource={dataSource} />
+      <Table tableLayout="auto" bordered columns={innerColumns} dataSource={dataSource} />
     </ReactDragListView.DragColumn>
   );
 }
@@ -91,10 +100,27 @@ export function TableDataAntd() {
   useInjectReducer({ key: 'tableDataAntd', reducer });
   useInjectSaga({ key: 'tableDataAntd', saga });
 
+  const [form] = Form.useForm();
+  const { Option } = Select;
+
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const toastConfigs = {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+  };
+
   const [tableData, setTableData] = useState(MockData.products);
   const [filterText, setFilterText] = useState('');
   const [minPrice, ] = useState(minBy(MockData.products, product => product.price).price);
   const [maxPrice, ] = useState(maxBy(MockData.products, product => product.price).price);
+  const [isProductAdderOpened, setProductAdderStatus] = useState(false);
 
   const handleFilterChange = evt => {
     setFilterText(evt.target.value);
@@ -102,12 +128,27 @@ export function TableDataAntd() {
   const handlePriceSliderChange = prices => {
     console.log(prices);
   };
+  const onSubmit = formData => {
+    console.log('formData: >>>', formData);
+    let clonedData = [...tableData];
+    clonedData.unshift({
+      id: uuid(),
+      ...formData
+    });
+    setTableData(clonedData);
+    setProductAdderStatus(false);
+    toast.success('You have added a product successfully!', toastConfigs);
+  };
 
   const handleFilterProductsbyPrice = prices => {
     console.log(prices);
     let clonedData = [...tableData];
     let filteredData = filter(clonedData, product => product.price <= prices[1] && product.price >= prices[0]);
     setTableData(filteredData);
+    //reset filtered data
+    if (isEqual(prices[0], minPrice) && isEqual(prices[1], maxPrice)) {
+      setTableData(MockData.products);
+    }
   };
 
   useEffect(() => {
@@ -118,6 +159,7 @@ export function TableDataAntd() {
         return (includes(product.name.toLowerCase(), filterText.toLowerCase()) || includes(product.type.toLowerCase(), filterText.toLowerCase));
       });
     } else {
+      //reset filtered data
       filteredData = MockData.products;
     }
     setTableData(filteredData);
@@ -125,9 +167,15 @@ export function TableDataAntd() {
 
   return (
     <TableDataAntdWrapper>
+      <ToastContainer />
+      <Row className="my-5 text-center">
+        <Col xs={24}>
+          <Button onClick={() => setProductAdderStatus(true)} shape="round" type="primary">Add new product</Button>
+        </Col>
+      </Row>
       <Row className="mt-5">
         <Col xs={24} md={12}>
-          <Input placeholder="Search by name, type,..." onChange={handleFilterChange} value={filterText} prefix={<SearchOutlined />} allowClear />
+          <Input autoFocus placeholder="Search by name, type,..." onChange={handleFilterChange} value={filterText} prefix={<SearchOutlined />} allowClear />
         </Col>
         <Col xs={24} md={12} className="px-5">
           <Slider 
@@ -136,13 +184,14 @@ export function TableDataAntd() {
             range 
             marks={{
               0: {label: <NumberFormat displayType="text" value={minPrice} thousandSeparator suffix=" đ" />},
-              7127123: {label: <NumberFormat displayType="text" value={maxPrice} thousandSeparator suffix=" đ" />}
+              7127123: {label: <NumberFormat style={{whiteSpace: 'nowrap'}} displayType="text" value={maxPrice} thousandSeparator suffix=" đ" />}
             }}
             max={maxPrice} 
             min={minPrice} 
             defaultValue={[minPrice, maxPrice]} 
             onAfterChange={handleFilterProductsbyPrice} 
-            onChange={handlePriceSliderChange} 
+            onChange={handlePriceSliderChange}
+            draggableTrack
           />
         </Col>
       </Row>
@@ -151,6 +200,70 @@ export function TableDataAntd() {
           <DraggableTable columns={columns} dataSource={tableData} />
         </Col>
       </Row>
+      <Modal
+        centered
+        visible={isProductAdderOpened}
+        onCancel={() => setProductAdderStatus(false)}
+        onOk={() => form.submit()}
+        title="Add new Product"
+      >
+        <Form
+          {...layout}
+          form={form}
+          onFinish={onSubmit}
+          name="control-hooks"
+        >
+          <AntdFormInput
+            name='name'
+            label="Name"
+            rules={[{ required: true, message: 'Error' }]}
+            hasFeedback
+          >
+            <Input />
+          </AntdFormInput>
+          <AntdFormInput
+            name='price'
+            label="Price"
+            rules={[{ required: true, message: 'Error' }]}
+            hasFeedback
+          >
+            <Input type="number" />
+          </AntdFormInput>
+          <Row>
+            <Col xs={24} md={12}>
+              <AntdFormInput
+                name="type"
+                label="Loại"
+              >
+                <Select defaultValue="different">
+                  <Option value="computer">Máy tính</Option>
+                  <Option value="washing">Máy giặt</Option>
+                  <Option value="car">Ô tô</Option>
+                  <Option value="motorbike">Xe máy</Option>
+                  <Option value="phone">Điện thoại</Option>
+                  <Option value="different">Khác</Option>
+                </Select>
+              </AntdFormInput>
+            </Col>
+            <Col xs={24} md={12}>
+            <AntdFormInput
+                name="origin"
+                label="Xuất xứ"
+              >
+                <Select defaultValue="DIFF">
+                  <Option value="US">Mỹ</Option>
+                  <Option value="JP">Nhật</Option>
+                  <Option value="VN">Việt Nam</Option>
+                  <Option value="EU">Châu Âu</Option>
+                  <Option value="KO">Hàn Quốc</Option>
+                  <Option value="DIFF">Khác</Option>
+                </Select>
+              </AntdFormInput>
+            </Col>
+          </Row>
+          
+        </Form>
+      </Modal>
     </TableDataAntdWrapper>
   );
 }
